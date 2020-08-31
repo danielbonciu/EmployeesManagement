009607129e45161f7b574684028b62b29d01e457
Daniel Bonciu <dbonciu@gmail.com>
2020-08-31 19:48:06 +0300

    first commit

---
+++ src/test/java/com/ausy_technologies/demo/EmployeesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ausy_technologies.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
